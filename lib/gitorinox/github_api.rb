61a98e77e02f661676082dbb9dd39a3f513d2731
jvillarejo <arzivian87@gmail.com>
2015-03-30 23:45:28 -0300

    added list

--- lib/gitorinox/github_api.rb
+++ lib/gitorinox/github_api.rb
@@ -1,7 +1,24 @@
 module Gitorinox
   class GithubAPI
-    def initalize(login, password)
+    def initialize(login, password)
       @client ||= Github.new(login: login, password: password)
     end
+
+    def matched_repository_names(match)
+      result = []
+      
+      @client.activity.watching.watched.each_page do |page|
+        page.each do |repo| 
+          if match
+            result << repo.name if repo.name.match(match)
+          else
+            result << repo.name
+          end
+        end
+      end
+
+      result
+    end
+
   end
 end
--- lib/gitorinox/list.rb
+++ lib/gitorinox/list.rb
@@ -2,23 +2,7 @@ module Gitorinox
   class List < Command
 
     def execute
-      @names = wathed_repositories_name
-      
-      if match
-        @names.select! { |n| n.match(match) }
-      end
-
-      @names.each { |n| puts n}
-    end
-
-    def wathed_repositories_name
-      result = []
-      github.activity.watching.watched.each_page do |page|
-        page.each do |repo| 
-          result << repo.name
-        end
-      end
-      result
+      github.matched_repository_names(match).each { |n| puts n}
     end
   end
 end
